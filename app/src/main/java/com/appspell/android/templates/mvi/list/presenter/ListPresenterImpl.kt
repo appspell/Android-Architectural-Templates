43b8003a0f71326995d0b68612be14d442f5de6e
Alexey Glukharev <alexey.gluharev@lazada.com>
2017-08-23 19:24:55 +0300

    unsubscribe interactor of presenter

--- app/src/main/java/com/appspell/android/templates/mvi/list/presenter/ListPresenterImpl.kt
+++ app/src/main/java/com/appspell/android/templates/mvi/list/presenter/ListPresenterImpl.kt
@@ -6,6 +6,7 @@ import com.appspell.android.templates.mvi.list.router.ListRouter
 import com.appspell.android.templates.mvi.list.view.ListView
 import com.appspell.android.templates.mvi.list.view.OnListItemClick
 import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.disposables.Disposable
 
 class ListPresenterImpl(
         val view: ListView,
@@ -13,18 +14,22 @@ class ListPresenterImpl(
         val router: ListRouter
 ) : ListPresenter, OnListItemClick {
 
+    lateinit var disposable: Disposable
+
     override fun init() {
         view.initViews(this)
     }
 
     override fun bind() {
-        interactor.requestList()!!
+        disposable = interactor.requestList()!!
                 .observeOn(AndroidSchedulers.mainThread())
                 .subscribe { list: List<DataEntity> -> view.updateList(list) }
     }
 
     override fun unBind() {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        if (!disposable.isDisposed) {
+            disposable.dispose()
+        }
     }
 
     override fun onItemClicked() {
